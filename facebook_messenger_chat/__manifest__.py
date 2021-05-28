7fd0392457091683d6b487804a36773772bf5081
harhu1234 <aashish.harhu@gmail.com>
2021-05-28 18:22:57 +0530

    [ADD] facebok messanger chat

---
+++ facebook_messenger_chat/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>
+#################################################################################
+{
+  "name"                 :  "Odoo Facebook Messenger Chat",
+  "summary"              :  """The Facebook Messenger chat widget will be visible in the odoo website.""",
+  "category"             :  "Website",
+  "version"              :  "1.0.0",
+  "sequence"             :  1,
+  "author"               :  "Webkul Software Pvt. Ltd.",
+  "license"              :  "Other proprietary",
+  "website"              :  "https://store.webkul.com/",
+  "description"          :  """https://webkul.com/blog/""",
+  "depends"              :  ['website'],
+  "data"                 :  [
+                             'views/res_config_views.xml',
+                             'views/templates.xml',
+                            ],
+  "demo"                 :  [],
+  "application"          :  True,
+  "installable"          :  True,
+  "auto_install"         :  False,
+
+  "pre_init_hook"        :  "pre_init_check",
+}

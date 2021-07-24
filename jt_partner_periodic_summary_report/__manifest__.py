8f6a862a7558c80ede1762ee11d7015104166638
harhu1234 <aashish.harhu@gmail.com>
2021-07-24 11:09:57 +0530

    [ADD] v14 profit loss and report html migration

---
+++ jt_partner_periodic_summary_report/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Jupical Technologies Pvt. Ltd.
+#    Copyright (C) 2018-TODAY Jupical Technologies(<http://www.jupical.com>).
+#    Author: Jupical Technologies Pvt. Ltd.(<http://www.jupical.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Partner Periodic Summary Report',
+    'summary': 'Partner Periodic Summary Report',
+    'version': '14.0.0.2.0',
+    'category': 'account',
+    'author': 'Jupical Technologies Pvt. Ltd.',
+    'maintainer': 'Jupical Technologies Pvt. Ltd.',
+    'website': 'http://www.jupical.com',
+    'license': 'LGPL-3',
+    'depends': ['account_reports'],
+    'data': [
+        'demo/demo.xml',
+        'views/account_move_line_view.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
